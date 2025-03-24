1edc7f39a5606d7f920d8aad7ce37011231c9ffe
Nabiweb <nabimondal123@.com>
2025-03-24 13:36:24 +0530

    server change 5

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors("http://localhost:3000/", "https://tsamplees.netlify.app/"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
