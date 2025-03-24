5c6cf73b7470a257057e9052c0f05e0764089128
Nabiweb <nabimondal123@.com>
2025-03-24 13:15:59 +0530

    server change 2 and env change 2

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors'); // Add this line
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
+app.use(cors()); // Enable CORS for all routes
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
